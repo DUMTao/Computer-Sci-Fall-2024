6e8374e0dc9f4e52bfb2a2fc82e8f47773a4746a
DUMTAO <kreyes@mocs.flsouthern.edu>
2024-10-28 12:38:18 -0400

    bruh

---
+++ ey.py
@@ -0,0 +1,8 @@
+def summation():
+    a = int(input('Enter Number: '))
+    b = int(input('Enter 2nd Number: '))
+    sum = a + b
+    return print(sum)
+
+summation()
+print(f'Tada! Here\'s your result! :D')
