060edc2ba48e97f7fe84b0920eb300d985d54da1
DUMTAO <kreyes@mocs.flsouthern.edu>
2024-11-18 09:33:41 -0500

    Oh no

--- labels.py
+++ labels.py
@@ -1,8 +1,20 @@
-import tkinter as tk
+import tkinter
 
-window = tk.Tk()
-temps = tk.Label(text='Temps are hot', foreground='blue')
+window = tkinter.Tk()
+temps = tkinter.Label(text='Name', foreground='blue', background='white', width=25, height=5)
 temps.pack()
+
+ent_username = tkinter.Entry()
+ent_username.pack()
+
+label_password = tkinter.Label(text='Password', foreground='blue', background='white', width=25, height=5)
+label_password.pack()
+
+cont_password = tkinter.Entry()
+cont_password.pack()
+
+btm_submit = tkinter.Button(text='Submit', width=10, height=3)
+
 
 
 
