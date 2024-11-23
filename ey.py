967501aa6c2a847c8918c2406d90dbc4f513ade4
Kamil Reyes <ky.reyes2120@gmail.com>
2024-11-22 20:11:06 -0500

    Cleaning up to make it easier to understand in this branch

--- ey.py
+++
@@ -1,8 +0,0 @@
-def summation():
-    a = int(input('Enter Number: '))
-    b = int(input('Enter 2nd Number: '))
-    sum = a + b
-    return print(sum)
-
-summation()
-print(f'Tada! Here\'s your result! :D')
