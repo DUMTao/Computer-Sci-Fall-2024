2c70e0f1fcea93ab44d605fe990ba2da4041c0e7
Kamil Reyes <ky.reyes2120@gmail.com>
2024-12-12 16:33:28 -0500

    starting over

--- Math_Calc/formulas.py
+++
@@ -1,5 +0,0 @@
-import math
-
-def Quatratic(a, b, c):
-        result = ( -(b) + math.sqrt((b ** 2) - (4 * a * c)) / (2 * a) )
-        return print(f'The answer to your quadratic formula is: {result}')
